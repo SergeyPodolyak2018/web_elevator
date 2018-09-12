b53ff409d247037ff9fa3a64096f43962e014a03
SergeyPodolyak2018 < sergey.podolyak2018@gmail.com>
2018-09-12 15:52:40 +0300

    Add gulp1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    cssmin = require('gulp-cssmin'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
 
-gulp.task('mytask', function() {
-    console.log('Привет, я таск!');
+gulp.task('watch', function () {
+    gulp.watch('./control/static/admin/less/*.less', ['less']);
 });
+
+gulp.task('less', function () {
+    gulp.src('./styles/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest('./styles/'))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./control/static/admin/css'))
+
+});
+
+gulp.task('default', ['less', 'watch']);
