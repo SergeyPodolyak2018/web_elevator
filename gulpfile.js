97cc04eaef318b3151d0eb3c9cc4ba0ef67862ba
SergeyPodolyak2018 < sergey.podolyak2018@gmail.com>
2018-10-16 09:31:59 +0300

    Add js concater

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,9 @@ const svgmin = require('gulp-svgmin');
 const uglify = require('gulp-uglify-es').default;
 const pump = require('pump');
 const gulpElevator = require('gulp-elevator');
+var concat = require('gulp-concat');
 
-
+//наблюдение за изменением файлов less
 gulp.task('watch', function () {
     gulp.watch('./control/static/less/*.less', ['less']);
 });
@@ -69,66 +70,72 @@ gulp.task('create', () => {
     });
 });
 
+//компиляция less файлов
 gulp.task('createStyle', function () {
     gulp.src('./control/static/less/*.css')
         .pipe(plumber())
         .pipe(less())
         .pipe(gulp.dest('./build/control/static/css'))
-        .pipe(cssmin())
-        .pipe(rename({
-            suffix: '.min'
-        }))
+        // .pipe(cssmin())
+        // .pipe(rename({
+        //     suffix: '.min'
+        // }))
         .pipe(gulp.dest('./build/control/static/css'))
 });
 
+//Минификация css
 gulp.task('minifyCss', function () {
     gulp.src('./control/static/css/*.css')
         .pipe(cssmin())
         .pipe(gulp.dest('./build/control/static/css'));
 });
 
+//минификация HTML
 gulp.task('minifyHtml', () => {
   return gulp.src('./control/*.html')
     .pipe(htmlmin({ collapseWhitespace: true,removeComments: true }))
     .pipe(gulp.dest('./build/control/'));
 });
 
+//копировать папки библиотек
 gulp.task('copyJsFolders', function () {
     gulp.src('./control/static/js/**?(.js)/**/**/**')
         .pipe(gulp.dest('./build/control/static/js'));
 });
 
+//сжатие файлов js
 gulp.task('compressJs', function () {
   return gulp.src("./control/static/js/*.js")
         .pipe(uglify(/* options */))
         .pipe(gulp.dest("./build/control/static/js"));
 });
 
+//копировать звуковые файлы
 gulp.task('copyAud', function () {
     gulp.src('./control/static/audio/*')
         .pipe(gulp.dest('./build/control/static/audio'));
 });
 
+//копировать шрифты
 gulp.task('copyFonts', function () {
     gulp.src('./control/static/fonts/*')
         .pipe(gulp.dest('./build/control/static/fonts'));
 });
 
+//Копировать все png картинки
 gulp.task('copyImg', function () {
     gulp.src('./control/static/img/*.png')
         .pipe(gulp.dest('./build/control/static/img'));
 });
 
+//копировать все svg картинки и минифицировать их
 gulp.task('copySVG', function () {
     gulp.src('./control/static/img/*.svg')
     .pipe(svgmin())
     .pipe(gulp.dest('./build/control/static/img'));
 });
 
-// gulp.task('copyImgProj', function () {
-//     gulp.src('./control/imgOfProject/*')
-//         .pipe(gulp.dest('./build/control/imgOfProject'));
-// });
+//копировать фаил проекта и минифицировать его
 gulp.task('copyImgProj', function () {
     return gulp.src('./control/static/imgOfProject/projectSVG.svg')
         .pipe(svgmin({
@@ -140,6 +147,7 @@ gulp.task('copyImgProj', function () {
         .pipe(gulp.dest('./build/control/static/imgOfProject'));
 });
 
+//копировать фафикон из контрол
 gulp.task('copyFav', function () {
     gulp.src('./control/*.png')
         .pipe(gulp.dest('./build/control'));
@@ -151,30 +159,28 @@ gulp.task('minifyHtmlMain', () => {
     .pipe(htmlmin({ collapseWhitespace: true,removeComments: true }))
     .pipe(gulp.dest('./build/'));
 });
+//Копировать фавиконы из крня
 gulp.task('copyFavMain', function () {
     gulp.src('./*.png')
         .pipe(gulp.dest('./build'));
 });
-// Подготовка папки старт
 
-// gulp.task('minifyHtmlStart', () => {
-//   return gulp.src('./start/*.html')
-//     .pipe(htmlmin({ collapseWhitespace: true,removeComments: true }))
-//     .pipe(gulp.dest('./build/start/'));
-// });
-// ulp.task('copyFavStart', function () {
-//     gulp.src('./start/*.png')
-//         .pipe(gulp.dest('./build/start'));
-// });
-// gulp.task('minifyCssStart', function () {
-//     gulp.src('./start/css/*.css')
-//         .pipe(cssmin())
-//         .pipe(gulp.dest('./build/start/css'));
-// });
+//Копия папки старт
 gulp.task('copyStart', function () {
     gulp.src('./start/**/*')
         .pipe(gulp.dest('./build/start'));
 });
+
+//Преобразовать svg corel в вид необходимый для работы проекта
+gulp.task('converter', () => {
+  return gulp.src('./control/static/imgOfProject/notConvertProject.svg')
+    .pipe(gulpElevator())
+    .pipe(rename("projectSVG.svg"))
+    .pipe(gulp.dest('./control/static/imgOfProject'));
+});
+
+
+
 
 gulp.task('buildStart', gulpSequence('copyStart'));
 gulp.task('buildControl', gulpSequence('converter','less','minifyHtml','minifyCss','copyJsFolders','compressJs','copyAud','copyFonts','copyImg','copySVG','copyImgProj','copyFav'));
@@ -183,10 +189,4 @@ gulp.task('build', gulpSequence('cleanFolder', 'create','minifyHtmlMain','copyFa
 
 
 
-gulp.task('converter', () => {
-  return gulp.src('./control/static/imgOfProject/notConvertProject.svg')
-    .pipe(gulpElevator())
-    .pipe(rename("projectSVG.svg"))
-    .pipe(gulp.dest('./control/static/imgOfProject'));
-});
 
